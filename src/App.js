268a8c9f6395ec49b20cf11aa569b5380813ac96
staryp <staryp3@gmail.com>
2018-07-18 02:20:09 +0900

    firebase 데이터 불러오기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import firebase from 'firebase';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+	constructor() {
+		super();
+		this.state = {
+			speed:50
+		}
+	}
+
+	componentDidMount() {
+		const dbRef = firebase.database().ref().child('speed');
+		dbRef.on('value', snapshot =>{
+			console.log("what..")
+			this.setState({
+				speed:snapshot.val()
+			})
+		})
+	}
+	render() {
+		
+
+		return (
+			<div className="App">
+				<h1>{this.state.speed}</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
+
+registerServiceWorker();
+var config = {
+	apiKey: "AIzaSyC2KDd7iMC6pVM2DmQe1dLliEB-4DYY4h0",
+	authDomain: "instaclone-27cb2.firebaseapp.com",
+	databaseURL: "https://instaclone-27cb2.firebaseio.com",
+	projectId: "instaclone-27cb2",
+	storageBucket: "instaclone-27cb2.appspot.com",
+	messagingSenderId: "1016814024091"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
