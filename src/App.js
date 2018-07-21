a9fb5d04ef3762dd6db67e844287a778d27206d1
staryp <staryp3@gmail.com>
2018-07-21 20:27:18 +0900

    로그인 제작 시작

--- src/App.js
+++ src/App.js
@@ -2,32 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './css/App.css';
 import firebase from 'firebase';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
 
 	constructor() {
 		super();
 		this.state = {
-			speed:50
+			speed:50,
+			email:"",
+			password:""
 		}
 	}
 
 	componentDidMount() {
 		const dbRef = firebase.database().ref().child('speed');
 		dbRef.on('value', snapshot =>{
-			console.log("what..")
-			this.setState({
+			this.setState({	
 				speed:snapshot.val()
 			})
 		})
 	}
 	render() {
-		
-
 		return (
 			<div className="App">
-				<h1>{this.state.speed}</h1>
+				<h1 className="input-group-prepend">{this.state.speed}</h1>
+				<div className="input-group">
+					<div className="input-group-prepend">
+						<span className="input-group-text" id="basic-addon1">id</span>
+						<input type="text" className="form-control" placeholder="id" aria-label="Username" aria-describedby="basic-addon1"/>
+					</div>
+					<div className="input-group-prepend">
+						<span className="input-group-text" id="basic-addon2">password</span>
+						<input type="text" className="form-control" placeholder="password" aria-label="Username" aria-describedby="basic-addon1"/>
+					</div>
+				</div>
 			</div>
 		);
 	}
